11d9e4aef47b8c27584b1e2e2e88cbdea244d2ea
Anton Morev <shikto@mail.ru>
2020-06-05 00:09:23 +0300

    #4.2 Migrate to TS. Create City type

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -1,36 +1,38 @@
+import { City } from '../types/city';
+
 module.exports = {
-  fetchCities () {
-    return [
-      {
-        id: 1,
-        title: 'Moscow'
-      },
-      {
-        id: 2,
-        title: 'Moscow'
-      }
-    ]
-  },
-  fetchCityById (id) {
-    return this.fetchCities().find(e => e.id === id)
-  },
-  fetchPeople () {
-    return [
-      {
-        id: 1,
-        name: 'Anton',
-        city_id: 2
-      },
-      {
-        id: 2,
-        name: 'Ivan',
-        city_id: 2
-      },
-      {
-        id: 3,
-        name: 'David',
-        city_id: 1
-      }
-    ]
-  }
-}
+    fetchCities(): City[] {
+        return [
+            {
+                id: 1,
+                title: 'Moscow'
+            },
+            {
+                id: 2,
+                title: 'Moscow'
+            }
+        ];
+    },
+    fetchCityById(id) {
+        return this.fetchCities().find(e => e.id === id);
+    },
+    fetchPeople() {
+        return [
+            {
+                id: 1,
+                name: 'Anton',
+                city_id: 2
+            },
+            {
+                id: 2,
+                name: 'Ivan',
+                city_id: 2
+            },
+            {
+                id: 3,
+                name: 'David',
+                city_id: 1
+            }
+        ];
+    }
+};
---
+++ src/types/city.ts
@@ -0,0 +1,4 @@
+export class City {
+    public id: number;
+    public title: string;
+}
