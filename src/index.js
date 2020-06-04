7436b3b442f03c266690f37e7e2056383739eadd
Anton Morev <shikto@mail.ru>
2020-06-05 00:04:10 +0300

    #4.1 Migrate to TS. Rename to TS

--- src/index.js
+++
@@ -1,8 +0,0 @@
-const fetcher = require('./lib/fetcher')
-
-console.log('Script started')
-const people = fetcher.fetchPeople()
-people.forEach(el => {
-  console.log('name is ', el.name)
-  console.log('city is ', fetcher.fetchCityById(el.city_id).title)
-})
