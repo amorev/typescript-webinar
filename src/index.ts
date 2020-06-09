94b154fe7a8977bd5783d8aa44dc5937e1d00c8c
Anton Morev <shikto@mail.ru>
2020-06-09 19:49:34 +0300

    #6 Express example

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
-import { Checker } from './checker/namespace';
-import CheckersFabric = Checker.CheckersFabric;
-import Checker200Data = Checker.Checker200Data;
-import Check200 = Checker.Check200;
+import * as bodyParser from 'body-parser';
+import loggerMiddleware from './middleware/logger';
+import HomeController from './controllers/HomeController';
+import App from './app/app';
 
-const fabric = new CheckersFabric();
-const checker200 = fabric.getChecker(Check200);
-const checkInfo: Checker200Data = {
-    url: "https://ya.ru"
-};
-checker200.checkSite(checkInfo)
-    .then(result => {
-        console.log(result);
-    });
+const app = new App({
+    port: 5000,
+    routes: [
+        new HomeController()
+    ],
+    middleWares: [
+        bodyParser.json(),
+        bodyParser.urlencoded({extended: true}),
+        loggerMiddleware
+    ]
+});
+
+app.listen();
