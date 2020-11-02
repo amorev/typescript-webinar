a98ac770d02304553de65e658474ec76d803e9ce
Anton Morev <shikto@mail.ru>
2020-11-02 21:09:54 +0300

    Generics Repository

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,49 @@
-import { CheckContains } from './lib/checker/constants';
-import { CheckerService } from './lib/checker/service';
+type Book = {
+    title: string,
+    author: string,
+}
 
-const service = new CheckerService;
-const result = service.checkSite(CheckContains, {
-    url: 'https://ya.ru',
-    info: 'Ya.Rum.gdsadsaetSetting'
+type Author = {
+    title: string,
+    age: number
+}
+
+class Repository<T> {
+    private elements: T[] = [];
+
+    public getElement<K extends keyof T>(key: K, value: any): T | undefined {
+        return this.elements.find(e => e[key] === value);
+    }
+
+    public addElement(book: T) {
+        this.elements.push(book);
+    }
+
+    public deleteElement<K extends keyof T>(key: K, value: any): void {
+        const index = this.elements.findIndex(el => el[key] === value);
+        if (index !== -1) {
+            this.elements.splice(index, 1);
+        }
+    }
+
+    public getElements(): T[] {
+        return this.elements;
+    }
+}
+
+const bookRepository = new Repository<Book>();
+bookRepository.addElement({
+    author: 'asdasd',
+    title: 'asdasd'
 });
-result.then(r => {
-    console.log(r);
+
+console.log(bookRepository.getElements());
+
+const AuthorRepository = new Repository<Author>();
+AuthorRepository.addElement({
+    age: 50,
+    title: "Author"
 });
+console.log(AuthorRepository.getElements());
+const el = AuthorRepository.getElement('title', 'Author')
+console.log(el);
--- src/lib/checker/constants.ts
+++
@@ -1,2 +0,0 @@
-export const Check200 = 'check200';
-export const CheckContains = 'checkContains';
