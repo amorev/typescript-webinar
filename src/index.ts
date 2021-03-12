4873efc7d53570d70b47991bbc9978eef10da0ae
Anton Morev <shikto@mail.ru>
2021-03-12 21:12:29 +0300

    Repository with generics

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,56 @@
-import { CheckersFabric, CheckerType } from './lib/checkers';
+type Book = {
+    title: string,
+    author: string,
+    pagesSize?: number,
+}
 
-const checker200 = CheckersFabric.GetChecker(CheckerType.check200);
-CheckersFabric.GetChecker(CheckerType.check200);
-CheckersFabric.GetChecker(CheckerType.check200);
-CheckersFabric.GetChecker(CheckerType.check200);
-CheckersFabric.GetChecker(CheckerType.check200);
-CheckersFabric.GetChecker(CheckerType.check200);
-CheckersFabric.GetChecker(CheckerType.check200);
-const checkHtml = CheckersFabric.GetChecker(CheckerType.checkHtml);
+type Author = {
+    name: string
+}
 
-checker200.check({
-    url: "https://ya.ru",
+class Repository<T> {
+    elements: T[] = [];
+
+    addElement(book: T): void {
+        this.elements.push(book);
+    }
+
+    getElements(): T[] {
+        return this.elements;
+    }
+
+    findElement<K extends keyof T>(key: K, value: T[K]): {
+        element: T
+    } {
+        return {
+            element: this.elements.find(e => e[key] === value)
+        };
+    }
+
+    deleteElement<K extends keyof T>(key: K, value: T[K]): void {
+        const index = this.elements.findIndex(e => e[key] === value);
+        if (index !== -1) {
+            this.elements.splice(index, 1);
+        }
+    }
+}
+
+const bookRepository = new Repository<Book>();
+bookRepository.addElement({
+    title: "Best book about typescript",
+    author: "Anton Morev"
 });
-checkHtml.check({
-    url: "https://ya.ru",
-    data: "123123"
+bookRepository.addElement({
+    title: "Not Best book about typescript",
+    author: "Not Anton Morev"
 });
+console.log(bookRepository.getElements(), 'before delete');
+bookRepository.deleteElement('author', "Best book about typescript");
+bookRepository.findElement('author', 'asd')
+console.log(bookRepository.getElements(), 'after delete');
+
+const authorRepository = new Repository<Author>();
+authorRepository.addElement({
+    name: 'asdasd'
+});
+authorRepository.findElement('name', 'asdads')
