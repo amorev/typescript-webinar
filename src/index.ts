fa96c6613940dd1716d638f5f798da83489896a3
Anton Morev <shikto@mail.ru>
2021-03-12 21:27:14 +0300

    Non working non nullable

--- src/index.ts
+++ src/index.ts
@@ -4,57 +4,18 @@ type Book = {
     pagesSize?: number,
 }
 
-type Author = {
-    name: string
+type BookWithoutPageSize = {
+    title: string,
+    author: string,
 }
 
-class Repository<T> {
-    elements: T[] = [];
+type Created = Book | null | undefined;
 
-    addElement(book: T): void {
-        this.elements.push(book);
-    }
-
-    getElements(): T[] {
-        return this.elements;
-    }
-
-    findElement<K extends keyof T>(key: K, value: T[K]): {
-        element: T
-    } {
-        return {
-            element: this.elements.find(e => e[key] === value)
-        };
-    }
-
-    deleteElement<K extends keyof T>(key: K, value: T[K]): void {
-        const index = this.elements.findIndex(e => e[key] === value);
-        if (index !== -1) {
-            this.elements.splice(index, 1);
-        }
-    }
-
-    updateElement<K extends keyof T>(key: K, value: T[K], newValue: Partial<T>): void {
-        const index = this.elements.findIndex(e => e[key] === value);
-        if (index !== -1) {
-            this.elements[index] = {
-                ...this.elements[index],
-                ...newValue
-            };
-        }
-    }
+function giveBook(): NonNullable<Created> {
+    return undefined;
 }
 
-const bookRepository = new Repository<Book>();
-bookRepository.addElement({
-    title: "Best book about typescript",
-    author: "Anton Morev"
-});
-bookRepository.addElement({
-    title: "Not Best book about typescript",
-    author: "Not Anton Morev"
-});
-bookRepository.updateElement('title', "Best book about typescript", {
-    author: "Max Ram"
-});
-console.log(bookRepository.getElements());
+const book = giveBook();
+console.log(book);
+
+const B: NonNullable<Book> = null;
