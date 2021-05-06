e1b9dc52aa6d31f3816ac0e6074cf317eba90ada
Anton Morev <shikto@mail.ru>
2021-05-06 20:19:49 +0300

    first class

--- index.ts
+++ index.ts
@@ -1,15 +1,10 @@
-let a: boolean = true;
+class Vehicle {
+    public go() {
+        console.log('i am going');
+    }
+}
 
-let b: any = 123;
-b = "23123";
-b = [];
-b = function () {
-};
 
-let arr: number[] = [1, 2, 3, 4, 5, 1];
+const car  = new Vehicle()
 
-let pair: [number, string] = [1, "asda"];
-
-const title: string = 'Anton';
-
-console.log(`Hello ${title}`);
+car.go();
