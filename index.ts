12f80c9331f972bba97df7938a720641dbe8b10d
Anton Morev <shikto@mail.ru>
2021-05-06 20:18:33 +0300

    Simple types

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log("Hello typescript");
+let a: boolean = true;
+
+let b: any = 123;
+b = "23123";
+b = [];
+b = function () {
+};
+
+let arr: number[] = [1, 2, 3, 4, 5, 1];
+
+let pair: [number, string] = [1, "asda"];
+
+const title: string = 'Anton';
+
+console.log(`Hello ${title}`);
