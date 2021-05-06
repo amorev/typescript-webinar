339e142fa44f1d16cd58d15165ce5a38afff2432
Anton Morev <shikto@mail.ru>
2021-05-06 21:00:30 +0300

    Basic fetcher on JS

--- src/compare.ts
+++
@@ -1,9 +0,0 @@
-export class Comparer {
-    public strings(a: string, b: string): boolean {
-        return a > b;
-    }
-
-    public numbers(a: number, b: number): boolean {
-        return a > b;
-    }
-}
