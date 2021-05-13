5f3e0565cfa75c1511ff470d1cea690a71100f19
Anton Morev <shikto@mail.ru>
2021-05-13 21:25:29 +0300

    Utility types

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,37 @@
+import Require = NodeJS.Require;
+
 type Book = {
     title: string,
     author: string,
     year?: number
 }
+
+type booksElements = "mine" | "other"
+const b: Readonly<Required<Book>> = {
+    author: "asd",
+    title: "asd",
+    year: 101
+};
+const books: Record<booksElements, Book> = {
+    mine: b,
+    other: b
+};
+type BookWithoutYear = Omit<Book, "author" | "title">
+const bookWithoutYear: BookWithoutYear = {
+    year: 123
+};
+
+function sth(): { title: string } {
+    return {
+        title: "asdas"
+    };
+}
+
+const element: ReturnType<typeof sth> = {
+    title: "dasd"
+};
+
+console.log(b);
 
 type Author = {
     name: string,
@@ -45,10 +74,10 @@ class Repository<T> {
     }
 }
 
-const b = new Repository<Author>();
-b.addElement({name: 'anton', city: 'moscow'});
-console.log(b.getElements());
-b.updateElement('name', 'anton', {
-    name: 'ivan'
-})
-console.log(b.getElements());
+// const b = new Repository<Author>();
+// b.addElement({name: 'anton', city: 'moscow'});
+// console.log(b.getElements());
+// b.updateElement('name', 'anton', {
+//     name: 'ivan'
+// })
+// console.log(b.getElements());
