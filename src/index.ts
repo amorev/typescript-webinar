b5c59942d60ee4eee264e7bbdc31360aa40bc3b7
Anton Morev <shikto@mail.ru>
2021-05-13 21:11:36 +0300

    Modified repository with generics

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,30 @@
 type Book = {
     title: string,
-    author: string
+    author: string,
+    year?: number
 }
 
-class BookRepository {
-    private _elements: Book[] = [];
+type Author = {
+    name: string,
+    city: string
+}
+
+class Repository<T> {
+    private _elements: T[] = [];
 
     public getElements() {
         return this._elements;
     }
 
-    public addElement(element: Book) {
+    public addElement(element: T) {
         this._elements.push(element);
     }
 
-    public getElement(key: string, value: any) {
+    public getElement<K extends keyof T>(key: K, value: T[K]): T {
         return this._elements.find(e => e[key] === value);
     }
 
-    public deleteElement(key: string, value: any) {
+    public deleteElement<K extends keyof T>(key: K, value: T[K]) {
         const index = this._elements.findIndex(e => e[key] === value);
         if (index !== -1) {
             this._elements.splice(index, 1);
@@ -26,16 +32,21 @@ class BookRepository {
     }
 }
 
+class BookRepository extends Repository<Book> {
+
+}
+
 const c = new BookRepository();
 c.addElement({
     author: "anton Morev",
-    title: "The best book about typescript"
+    title: "The best book about typescript",
+    year: 2021
 });
 c.addElement({
     author: "SomebodyMore",
-    title: "Not the best book about typescript"
+    title: "Not the best book about typescript",
+    year: 2011
 });
 console.log(c.getElements());
 console.log(c.getElement('author', 'anton Morev'));
-console.log(c.deleteElement('author', 'SomebodyMore'));
-console.log(c.getElements());
+console.log(c.getElement('year', 2021));
