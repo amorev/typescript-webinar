e3b61cc713706221e705b677c849c4c54750ff4b
Anton Morev <shikto@mail.ru>
2021-05-13 21:32:52 +0300

    decorators

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,33 @@
-import Require = NodeJS.Require;
+function logger(target, key, descriptor) {
+    const beforeMethod = descriptor.value
+    descriptor.value = function () {
+        console.log('log', new Date());
+        return beforeMethod.apply(this, arguments)
+    }
+
+    return descriptor
+}
+
 
 type Book = {
     title: string,
     author: string,
     year?: number
 }
-
-type booksElements = "mine" | "other"
-const b: Readonly<Required<Book>> = {
-    author: "asd",
-    title: "asd",
-    year: 101
-};
-const books: Record<booksElements, Book> = {
-    mine: b,
-    other: b
-};
-type BookWithoutYear = Omit<Book, "author" | "title">
-const bookWithoutYear: BookWithoutYear = {
-    year: 123
-};
-
-function sth(): { title: string } {
-    return {
-        title: "asdas"
-    };
-}
-
-const element: ReturnType<typeof sth> = {
-    title: "dasd"
-};
-
-console.log(b);
-
 type Author = {
     name: string,
     city: string
 }
+function log() {
+    console.log('log', new Date());
 
-// console.log(a);
+}
 
 
 class Repository<T> {
     private _elements: T[] = [];
 
+    @logger
     public getElements() {
         return this._elements;
     }
@@ -63,6 +47,7 @@ class Repository<T> {
         }
     }
 
+    @logger
     public updateElement<K extends keyof T>(key: K, value: T[K], newValue: Partial<T>) {
         const index = this._elements.findIndex(e => e[key] === value);
         if (index !== -1) {
@@ -74,10 +59,10 @@ class Repository<T> {
     }
 }
 
-// const b = new Repository<Author>();
-// b.addElement({name: 'anton', city: 'moscow'});
-// console.log(b.getElements());
-// b.updateElement('name', 'anton', {
-//     name: 'ivan'
-// })
-// console.log(b.getElements());
+const b = new Repository<Author>();
+b.addElement({name: 'anton', city: 'moscow'});
+console.log(b.getElements());
+b.updateElement('name', 'anton', {
+    name: 'ivan'
+})
+console.log(b.getElements());
