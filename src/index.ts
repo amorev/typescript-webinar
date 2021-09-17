9f1c8c1528dc8995636541cf73ace4d9227fe680
Anton Morev <shikto@mail.ru>
2021-09-17 20:42:36 +0300

    Simple express app

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,35 @@
 import axios from 'axios';
 import { CheckersFabric } from './checker';
 
+const express = require('express');
+const app = express();
+const port = 3000;
+
 async function run() {
-    let url = 'https://ya.ru';
-    let info = {url, checkData: "browser"};
-    const checker200 = CheckersFabric.getChecker('200');
-    const result = await checker200.checkSite({url});
-    console.log(result);
+
+
+    app.get('/', (req, res) => {
+        res.send('Hello World!');
+    });
+    app.get('/checker', async (req, res) => {
+        const checker = CheckersFabric.getChecker(req.query.type);
+        const result = await checker.checkSite({
+            url: req.query.url,
+            checkData: req.query.data
+        });
+        res.send(result);
+    });
+
+    app.listen(port, () => {
+        console.log(`Example app listening at http://localhost:${port}`);
+    });
+
+
+    // let url = 'https://ya.ru';
+    // let info = {url, checkData: "browser"};
+    // const checker200 = CheckersFabric.getChecker('200');
+    // const result = await checker200.checkSite({url});
+    // console.log(result);
 }
 
 run();
