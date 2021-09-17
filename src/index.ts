f7665cb0fd55d936334ed747b1eddc8f5ce15672
Anton Morev <shikto@mail.ru>
2021-09-17 21:06:15 +0300

    Very simple decorator

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,59 @@
-import { App } from './app';
-import { CheckerController } from './controllers/CheckerController';
-import { NewController } from './controllers/NewController';
+// import { App } from './app';
+// import { CheckerController } from './controllers/CheckerController';
+// import { NewController } from './controllers/NewController';
+//
+// async function run() {
+//     const Application = new App({
+//         port: 3000,
+//         routes: [
+//             new CheckerController(),
+//             new NewController()
+//         ]
+//     });
+//     Application.listen();
+//     // app.get('/', (req, res) => {
+//     //     res.send('Hello World!');
+//     // });
+//     // app.get('/checker', async (req, res) => {
+//     //     const checker = CheckersFabric.getChecker(req.query.type);
+//     //     const result = await checker.checkSite({
+//     //         url: req.query.url,
+//     //         checkData: req.query.data
+//     //     });
+//     //     res.send(result);
+//     // });
+//
+//     // app.listen(port, () => {
+//     //     console.log(`Example app listening at http://localhost:${port}`);
+//     // });
+//
+//
+//     // let url = 'https://ya.ru';
+//     // let info = {url, checkData: "browser"};
+//     // const checker200 = CheckersFabric.getChecker('200');
+//     // const result = await checker200.checkSite({url});
+//     // console.log(result);
+// }
+//
+// run();
 
-async function run() {
-    const Application = new App({
-        port: 3000,
-        routes: [
-            new CheckerController(),
-            new NewController()
-        ]
-    });
-    Application.listen();
-    // app.get('/', (req, res) => {
-    //     res.send('Hello World!');
-    // });
-    // app.get('/checker', async (req, res) => {
-    //     const checker = CheckersFabric.getChecker(req.query.type);
-    //     const result = await checker.checkSite({
-    //         url: req.query.url,
-    //         checkData: req.query.data
-    //     });
-    //     res.send(result);
-    // });
 
-    // app.listen(port, () => {
-    //     console.log(`Example app listening at http://localhost:${port}`);
-    // });
-
-
-    // let url = 'https://ya.ru';
-    // let info = {url, checkData: "browser"};
-    // const checker200 = CheckersFabric.getChecker('200');
-    // const result = await checker200.checkSite({url});
-    // console.log(result);
+function my(t, k, d) {
+    console.log('my');
+    const originalMethod = d.value
+    d.value = function () {
+        console.log('my method!');
+        originalMethod.apply(this, arguments)
+    }
 }
 
-run();
+class A {
+    @my
+    method() {
+        console.log('method');
+    }
+}
+
+console.log('before');
+
+(new A).method();
